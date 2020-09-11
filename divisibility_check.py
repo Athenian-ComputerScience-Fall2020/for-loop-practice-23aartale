42e43729e10eb9077a7253195736a657b5f7557f
23aartale <23aartale@athenian.org>
2020-09-11 17:22:35 +0000

    fixed it

--- divisibility_check.py
+++ divisibility_check.py
@@ -1,3 +1,9 @@
 # Collaborators: 
-#
+#none 
+for x in range (2, 51):
+    if x % 3  == 0:
+        print(str(x) + " is divisble by 3")
+
+    else:
+        print(str(x) + " is not divisble by 3")
 
